40f112a66188d5c1e6439483b4e965bdf3e394c4
Paul Rossotti <rossotti.paul@gmail.com>
2016-09-07 18:26:29 -0700

    updates to applicationContext

--- src/main/java/com/rossotti/basketball/app/main/ScheduleByGameDate.java
+++ src/main/java/com/rossotti/basketball/app/main/ScheduleByGameDate.java
@@ -1,21 +1,19 @@
 package com.rossotti.basketball.app.main;
 
-import com.rossotti.basketball.app.gateway.GameActivator;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.rossotti.basketball.app.gateway.GatewayService;
 import com.rossotti.basketball.dao.model.Game;
-import com.rossotti.basketball.dao.model.GameDay;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ScheduleByGameDate {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {
 				"classpath:si-config.xml",
-				"classpath:applicationContext.xml"
+				"classpath:applicationContext_MySql.xml"
 		});
 		System.out.println("begin gatewayService");
 		GatewayService gatewayService = (GatewayService) context.getBean("gatewayService");
-		Game game = gatewayService.processGames("2015-01-07");
+		Game game = gatewayService.processGames("2015-11-06");
 		System.out.println("end gatewayService " + game.getBoxScoreHome().getTeam().getTeamKey());
 		context.close();
 	}
--- src/test/java/com/rossotti/basketball/dao/repository/GameRepositoryTest.java
+++ src/test/java/com/rossotti/basketball/dao/repository/GameRepositoryTest.java
@@ -9,7 +9,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 

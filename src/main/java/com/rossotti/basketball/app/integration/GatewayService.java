e052a3f95a2ea89a7b3b2a0ab7bd5d66281d355d
Paul Rossotti <rossotti.paul@gmail.com>
2016-10-07 17:40:50 -0700

    change gameActivatorChannel to inputChannel

--- src/main/java/com/rossotti/basketball/app/integration/GatewayService.java
+++ src/main/java/com/rossotti/basketball/app/integration/GatewayService.java
@@ -7,6 +7,6 @@ import org.springframework.integration.annotation.Gateway;
 import com.rossotti.basketball.dao.model.Game;
 
 public interface GatewayService {
-	@Gateway(requestChannel = "gameActivatorChannel")
+	@Gateway(requestChannel = "inputChannel")
 	List<Game> processGames(ServiceProperties serviceProperties);
 }

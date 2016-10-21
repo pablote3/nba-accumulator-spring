3305b4c5c6651a27d8d1b60e0448a62d6be7b7dd
Paul Rossotti <rossotti.paul@gmail.com>
2016-10-21 10:38:39 -0700

    suppress CanBeFinal warnings on DTOs

--- src/main/java/com/rossotti/basketball/client/dto/RosterDTO.java
+++ src/main/java/com/rossotti/basketball/client/dto/RosterDTO.java
@@ -1,5 +1,6 @@
 package com.rossotti.basketball.client.dto;
 
+@SuppressWarnings("CanBeFinal")
 public class RosterDTO extends StandingsDTO {
 	public TeamDTO team;
 	public RosterPlayerDTO[] players;
